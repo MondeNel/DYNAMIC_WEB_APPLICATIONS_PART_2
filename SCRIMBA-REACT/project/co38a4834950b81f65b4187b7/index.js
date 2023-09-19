ab27e2af709604b18ba54422e700fadaaadf88cd
Monde Nel <mondenel1996@gmail.com>
2023-09-19 13:11:16 +0200

    created a functional component for NavBar

--- SCRIMBA-REACT/project/co38a4834950b81f65b4187b7/index.js
+++ SCRIMBA-REACT/project/co38a4834950b81f65b4187b7/index.js
@@ -1,17 +1,33 @@
 
 
-/*
-Challenge - recreate the above line of code in vanilla JS by creating and
-appending an h1 to our div#root (without using innerHTML).
+/* 
+Challenge: 
 
-- Create a new h1 element
-- Give it some textContent
-- Give it a class name of "header"
-- append it as a child of the div#root
-
+Create a navbar in JSX:
+    - Use the semantic `nav` element as the parent wrapper
+    - Have an h1 element with the brand name of your "website"
+    - Insert an unordered list for the other nav elements
+        - Inside the `ul`, have three `li`s for "Pricing",
+        "About", and "Contact"
+    - Don't worry about styling yet - it'll just be plain-looking HTML for now
 */
 
-const h1 = document.createElement("h1");
-h1.textContent = "This is an imperative way to program"
-h1.className = "header"
-document.getElementById("root").append(h1);
+
+// This is more like a functional componet
+
+function NavBar() {
+    return (
+
+        <div>
+            <h1>Website</h1>
+            <ul>
+                <li>Pricing</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+
+        </div>
+    )
+}
+
+ReactDOM.render(<NavBar />, document.getElementById("root"))

d15df1cdda41d546982ab288ae8b47843f7e99c6
Monde Nel <mondenel1996@gmail.com>
2023-09-19 12:35:35 +0200

    unzipped the project file from scrimba

---
+++ SCRIMBA-REACT/project/co38a4834950b81f65b4187b7/index.js
@@ -0,0 +1,14 @@
+/* Surprise! You probably thought you could just forget
+the line of code you just learned! Nope, not on my watch!
+
+Try to write that 1-liner of React code again! This time,
+see if you can figure out how to render an <ul> with 2+ <li>s inside*/
+
+
+ReactDOM.render(
+    <ul>
+        <li>React is cool</li>
+        <li>React is maintained by the Facebook team</li>
+    </ul>,
+    document.getElementById("root")
+)

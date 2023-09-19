5c24d64ca33848d52423a7a7a3726f4dcd9a02f7
Monde Nel <mondenel1996@gmail.com>
2023-09-19 12:29:57 +0200

    rendered an unordered list in the browser with ReactDOM

--- SCRIMBA-REACT/scripts.js
+++ SCRIMBA-REACT/scripts.js
@@ -1,1 +1,12 @@
-ReactDOM.render(<h1>Hello world</h1>, document.getElementById("root"));
+
+
+/* Try to write that 1 - liner of React code again! This time,
+    see if you can figure out how to render an < ul > with 2 + <li>s inside*/
+
+
+ReactDOM.render(
+    <ul>
+        <li>React is cool!</li>
+        <li>React is maintained by Facebook</li>
+    </ul>,
+    document.getElementById("root"));

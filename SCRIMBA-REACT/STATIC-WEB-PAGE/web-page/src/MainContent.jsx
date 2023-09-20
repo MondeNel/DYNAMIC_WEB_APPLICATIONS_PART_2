03324a79da026aec9cde1701789fd2d47680082a
Monde Nel <mondenel1996@gmail.com>
2023-09-20 13:03:24 +0200

    added the mainContent component with some styling

--- SCRIMBA-REACT/STATIC-WEB-PAGE/web-page/src/MainContent.jsx
+++ SCRIMBA-REACT/STATIC-WEB-PAGE/web-page/src/MainContent.jsx
@@ -5,15 +5,17 @@ const MainContent = () => {
   return (
 
     <div className='main-content'>
-      <h1>Reasons I'm excited to learn React</h1>
 
-      <ol>
+
+      <h1 className='main-title'>Fun facts about React!</h1>
+
+      <ul className='main-list'>
         <li>Was first released in 2013</li>
         <li>Was originally created by Jordan Walke</li>
         <li>Has well over 100K stars on Github</li>
         <li>Is maintained by Facebook</li>
         <li>Powers thousands of enterprise apps, including mobile apps</li>
-      </ol>
+      </ul>
 
     </div>
   )

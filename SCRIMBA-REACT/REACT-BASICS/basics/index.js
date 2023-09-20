293c62fb1f64f84e5503583e5059ec7109a39142
Monde Nel <mondenel1996@gmail.com>
2023-09-20 14:25:44 +0200

    added a js react project for the first challenge

--- SCRIMBA-REACT/REACT-BASICS/basics/index.js
+++ SCRIMBA-REACT/REACT-BASICS/basics/index.js
@@ -2,17 +2,43 @@
 
 function NavBar() {
     return (
-
         <div>
-            <h1>Website</h1>
-            <ul>
-                <li>Pricing</li>
-                <li>About</li>
-                <li>Contact</li>
-            </ul>
-
+            <img src="./image/react-logo.png" alt="React Logo" width="40px" />
+            <h1>Reasons I'm excited to learn React</h1>
         </div>
     )
 }
 
-ReactDOM.render(<NavBar />, document.getElementById("root"))
+function Main() {
+    return (
+        <div>
+            <ol>
+                <li>It's a popular library, so I'll beable to fit in with the cool kids!</li>
+                <li>I'm more likely to get a job as a developer if I know React</li>
+            </ol>
+        </div>
+    )
+}
+
+function Footer() {
+    return (
+        <footer>
+            © 20xx development. All rights reserved
+        </footer>
+    )
+}
+
+
+
+function App() {
+    return (
+        <div>
+            <NavBar />
+            <Main />
+            <Footer />
+        </div>
+    );
+}
+
+
+ReactDOM.render(<App />, document.getElementById("root"))

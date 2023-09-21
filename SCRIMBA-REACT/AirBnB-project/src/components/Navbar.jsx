f7a0d3edaaaf814315c418fb1a0bc61b41336690
Monde Nel <mondenel1996@gmail.com>
2023-09-21 17:18:41 +0200

    added my vite-project(Airbnb) and added the navbar with some styling and the logo

---
+++ SCRIMBA-REACT/AirBnB-project/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Navbar = () => {
+    return (
+        <nav>
+            <img src="./src/images/airbnb.png" alt="AirBnB logo" />
+        </nav>
+    )
+}
+
+export default Navbar

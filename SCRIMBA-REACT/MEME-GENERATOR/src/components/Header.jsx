6514018b8b7243d49b4e4b2ee6aafe2a81853108
Monde Nel <mondenel1996@gmail.com>
2023-09-28 10:52:27 +0200

    created a new project(Meme generator) and added the header component and also added some css styling

---
+++ SCRIMBA-REACT/MEME-GENERATOR/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+            <nav className='nav'>
+                <img src="/Troll Face.png" className="Meme-logo" alt="Troll face logo" />
+                <h2 className='logo-text'>Meme Generator</h2>
+                <h3 className='title-text'>React Course - Project 3</h3>
+            </nav>
+        </header>
+    )
+}
+
+export default Header

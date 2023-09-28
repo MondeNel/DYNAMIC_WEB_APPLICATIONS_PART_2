97aa9c74dad689c899de00e34ae90d123bfbdd06
Monde Nel <mondenel1996@gmail.com>
2023-09-28 12:14:16 +0200

    added some data for the Meme generator that would allow us to extract data and display jokes onto the screen. I've also some logic that would allow us to run a function when the button get's clicked

--- SCRIMBA-REACT/MEME-GENERATOR/src/components/Meme.jsx
+++ SCRIMBA-REACT/MEME-GENERATOR/src/components/Meme.jsx
@@ -1,31 +1,42 @@
 import React from 'react'
+import MemeData from '../MemeData';
+
 
 const Meme = () => {
+
+    function getMemeImage() {
+        const { memes } = MemeData.data;
+        const randomIndex = Math.floor(Math.random() * memes.length);
+        const randomMeme = memes[randomIndex];
+        const memeUrl = randomMeme.url;
+        console.log(memeUrl);
+    }
+
     return (
         <main>
-
             <div className='form-container'>
-                <form className="form">
-                    <input
-                        type="text"
-                        placeholder="Top text"
-                        className="form-input"
-                    />
-                    <input
-                        type="text"
-                        placeholder="Bottom text"
-                        className="form-input"
-                    />
-                    <button
-                        className="form-button"
-                    >
-                        Get a new meme image 🖼
-                    </button>
-                </form>
+                <input
+                    type="text"
+                    placeholder="Top text"
+                    className="form-input"
+                />
+                <input
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form-input"
+                />
+                <button
+                    className="form-button"
+                    onClick={getMemeImage}
+
+                >
+                    Get a new meme image 🖼
+                </button>
+
 
             </div>
 
-        </main>
+        </main >
     )
 }
 
